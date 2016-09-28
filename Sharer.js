f3f81fcdc3b39c16d8654baaad1e8cd698ef9409
Konstantin Zamyakin <dev@weblab.pro>
2016-09-28 03:09:25 +0300

    updated sharer

--- Sharer.js
+++ Sharer.js
@@ -1,63 +1,93 @@
 "use strict";
 function Sharer (options) {
-	options = options || {};
-	var self = this;
+    options = options || {};
+    var self = this;
 
-	var def = {
-		"vk": "https://vk.com/share.php?url=",
-		"fb": "http://facebook.com/sharer/sharer.php?u=",
-		"tw": "https://twitter.com/intent/tweet?url="
-	};
+    var def = {
+        "vk": "https://vk.com/share.php?url=",
+        "fb": "https://www.facebook.com/dialog/share",
+        "tw": "https://twitter.com/intent/tweet"
+    };
 
-	this.share_vk = function () {
-		this._openPopup(def.vk + window.location.href, "vk");
-	}
-	this.share_fb = function () {
-		this._openPopup(def.fb + window.location.href, "fb");
-	}
-	this.share_tw = function () {
-		var meta_title = document.querySelector("meta[property='og:title']");
-		var text = document.title;
+    this.share_vk = function () {
+        this._openPopup(def.vk + window.location.href, "vk");
+    }
 
-		if (meta_title) {
-			text = meta_title.getAttribute("content");
-		}
+    this.share_fb = function () {
+        var meta_app_id = document.querySelector("meta[property='fb:app_id']");
+        var qs = "?display=page";
 
-		this._openPopup(def.tw + window.location.href + "&text=" + text, "tw");
-	}
+        if (meta_app_id) {
+            qs += "&app_id=" + meta_app_id.getAttribute("content");
+        }
 
-	this._openPopup = function (url, name) {
-		var w = 620;
-		var h = 430;
-		var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
-		var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+        qs += "&href=" + window.location.href;
 
-		var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
-		var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+        this._openPopup(def.fb + qs, "fb");
+    }
 
-		var left = ((width / 2) - (w / 2)) + dualScreenLeft;
-		var top = ((height / 2) - (h / 2)) + dualScreenTop;
-		var newWindow = window.open(url, name + ' Share', 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+    this.share_tw = function () {
+        var meta_title = document.querySelector("meta[property='og:title']");
+        var meta_creator = document.querySelector("meta[name='twitter:creator']");
 
-		if (window.focus) {
-			newWindow.focus();
-		}
-	}
+        var text = document.title;
+        var url = window.location.href,
+            qs = "?url=" + url,
+            via;
 
-	this.bindEvents = function () {
-		var self = this;
+        if (meta_title) {
+            text = meta_title.getAttribute("content");
+        }
 
-		Object.keys(def).forEach(function (network) {
-			var selector = "[data-share='" + network + "']";
-			document.querySelectorAll(selector).forEach(function(element) {
-				element.addEventListener("click", self["share_" + network].bind(self));
-			});
-		});
-	}
+        if (meta_creator) {
+            via = meta_creator.getAttribute("content").replace('@', '');
+        }
 
-	this.run = function() {
-		this.bindEvents();
-	}.bind(this)
+        if (text) {
+            qs += "&text=«" + encodeURIComponent(text) + "»";
+        }
+
+        if (via) {
+            qs += "&via=" + via;
+        }
+
+        this._openPopup(def.tw + qs, "tw");
+    }
+
+    this._openPopup = function (url, name) {
+        console.log(url);
+        var w = 620;
+        var h = 430;
+        var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
+        var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+
+        var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+        var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+        var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+        var top = ((height / 2) - (h / 2)) + dualScreenTop;
+        var newWindow = window.open(url, name + ' Share', 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+
+        if (window.focus) {
+            newWindow.focus();
+        }
+    }
+
+    this.bindEvents = function () {
+        var self = this;
+
+        Object.keys(def).forEach(function (network) {
+            var selector = "[data-share='" + network + "']";
+
+            document.querySelectorAll(selector).forEach(function(element) {
+                element.addEventListener("click", self["share_" + network].bind(self));
+            });
+        });
+    }
+
+    this.run = function() {
+        this.bindEvents();
+    }.bind(this)
 };
 
 module.exports = Sharer;
